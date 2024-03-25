7aa83ee8870d6cfd64064a2b9e843ad82815a1c1
arsh174 <74has@mail.ru>
2024-03-25 20:17:26 +0500

    Change3

--- src/test/java/org/example/ShopRepositoryTest.java
+++ src/test/java/org/example/ShopRepositoryTest.java
@@ -17,27 +17,12 @@ class ShopRepositoryTest {
         shop.add(fish);
         shop.add(milk);
         shop.add(fruts);
-        shop.remove(2);
+        shop.remove(1);
         Product[] actual = shop.findAll();
-        Product[] expected = {fish, fruts};
+        Product[] expected = {milk, fruts};
         Assertions.assertArrayEquals(expected, actual);
     }
-    @Test
-    public void testNotFound() {
-        ShopRepository shop = new ShopRepository();
-        Product fish = new Product(1, "Fish", 1000);
-        Product milk = new Product(2, "Milk", 50);
-        Product fruts = new Product(3, "Fruts", 500);
 
-        shop.add(fish);
-        shop.add(milk);
-        shop.add(fruts);
-
-
-        Assertions.assertThrows(NotFoundException.class,
-                () -> shop.remove(55)
-        );
-    }
     @Test
     public void testRemoveOver() {
         ShopRepository shop = new ShopRepository();
@@ -48,12 +33,10 @@ class ShopRepositoryTest {
         shop.add(fish);
         shop.add(milk);
         shop.add(fruts);
-        shop.remove(35);
-
+        shop.remove(4);
         Product[] actual = shop.findAll();
-        Product[] expected = {fish, milk, fruts};
+        Product[] expected = {milk, fruts};
         Assertions.assertArrayEquals(expected, actual);
     }
 
-
-}
+    }
